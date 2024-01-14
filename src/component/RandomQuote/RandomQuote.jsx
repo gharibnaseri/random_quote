20637d373f0951339a16b765b0ca017b60f62793
mojtaba <110784968+gharibnaseri@users.noreply.github.com>
2024-01-14 01:41:30 -0500

    make the quote responsible

--- src/component/RandomQuote/RandomQuote.jsx
+++ src/component/RandomQuote/RandomQuote.jsx
@@ -1,39 +1,35 @@
-import React , {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import "./RandomQuote.css"
+import { FaTwitter, FaQuoteLeft, FaQuoteRight } from 'react-icons/fa'
 const RandomQuote = () => {
-    const [quote, setQuote]=useState('')
-    const [author, setAuthor]=useState('')
+const [quote, setQuote] = useState('')
+const [author, setAuthor] = useState('')
 
-    // http://api.quotable.io/random
 
-    useEffect(()=>
-    {
-        fetch("http://api.quotable.io/random")
-        .then(res=> res.json())
-        .then((quote)=> {
-            setAuthor(quote.author);
-            setQuote(quote.content);
-     })
-    
-    }
-    ,[])
 
-    let generateNewTweet= ()=>{
-        fetch("http://api.quotable.io/random")
-        .then(res=> res.json())
-        .then((quote)=> {
-            setAuthor(quote.author);
-            setQuote(quote.content);
-    })
-    }
+  useEffect(() => {
+    generateNewTweet()
+  }
+    , [])
+
+  let generateNewTweet = () => {
+    fetch("http://api.quotable.io/random")
+      .then(res => res.json())
+      .then((quote) => {
+        setAuthor(quote.author);
+        setQuote(quote.content);
+      })
+  }
 
   return (
     <div className='App'>
       <div className='Quote'>
+        <FaQuoteLeft className='iconLeft' />
         <h2>{quote}</h2>
+        <FaQuoteRight className='iconRight' />
         <small>-{author}-</small>
       </div>
-      <br/>
+      <br />
       <button className='btn' onClick={generateNewTweet}> Generate New Quote </button>
     </div>
   )
